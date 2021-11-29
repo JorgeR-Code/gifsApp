8fe43d871cfeb8126782334237adb4dd4e4eaba9
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-11-28 19:24:24 -0600

    Local Storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+private api: string = 'D9a5i6eBAuxf3DWLujbPWJoMEvxwMNf4';
 private _historial: string[]=[];
+
+public resultados: Gif[] = [];
 
 get historial(){
   return [...this._historial];
 }
 
-buscarGifs(query: string){
-  this._historial.unshift(query);
-  console.log(this._historial);
+constructor(private http: HttpClient){
+
+this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+// if(localStorage.getItem('historial')){
+//   this._historial = JSON.parse(localStorage.getItem('historial')!);
+// }
 }
+
+buscarGifs(query: string){
+
+query = query.trim().toLocaleLowerCase();
+
+  if(query){
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=D9a5i6eBAuxf3DWLujbPWJoMEvxwMNf4&q=${query}&limit=10`).subscribe((resp) => {
+      this.resultados = resp.data;
+    });
+
+  }else{
+    return
+  }
+};
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  get historial(){
+    return  this.gifService.historial;
   }
+
+  constructor(private gifService:GifsService) {
+
+  }
+
 
 }
